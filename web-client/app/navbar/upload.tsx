2d12f5ccc8c909f59269966a6eb53b1793695cb9
tahsin <tahsinbd.ahmed1@gmail.com>
2023-12-30 19:34:22 +0600

    upload icon added in the ui

--- web-client/app/navbar/upload.tsx
+++ web-client/app/navbar/upload.tsx
@@ -9,16 +9,12 @@ import styles from './upload.module.css';
 export default function Upload() {
     return (
         <Fragment>
-            <input
-                type="file"
-                title="Upload a video"
-                accept="video/*"
-                className={styles.fileInput}
-                onChange={async (e) => {
-                    const file = e.target.files?.item(0);
-                    if(file) await uploadFile(file);
-                }}
-            />
+            <input id ="upload" type="file" accept="video/*" className={styles.inputnone} />
+            <label htmlFor="upload" className={styles.fileInput}>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.1} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15" />
+</svg> 
+            </label>
         </Fragment>
     );
 }
